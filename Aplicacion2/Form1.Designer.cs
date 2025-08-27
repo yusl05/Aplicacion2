70824e0acf2999f67b6f158a30c82c66b26bfdec
yusl05 <jesed2005ma@gmail.com>
2025-08-27 15:25:33 -0600

    Se agregaron botones

--- Aplicacion2/Form1.Designer.cs
+++ Aplicacion2/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BOTON1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(310, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "BOTON2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
